7c27e73079d4ad6e825eb2d4d7016f1445f5d0e2
jonandonigv <jonandonigv@gmail.com>
2026-03-11 18:19:27 +0100

    wip: fswatch

--- cmd/filesync/main.go
+++ cmd/filesync/main.go
@@ -3,8 +3,27 @@ Copyright © 2026 Jon Andoni Galdos <jonandonigv@gmail.com>
 */
 package main
 
-import "github.com/jonandonigv/FileSync/cmd/filesync/cmd"
+import (
+	_ "fmt"
+	"log"
+
+	_ "github.com/jonandonigv/FileSync/cmd/filesync/cmd"
+	"github.com/rjeczalik/notify"
+)
+
+func fsWatch() {
+	c := make(chan notify.EventInfo, 1)
+	err := notify.Watch("./...", c, notify.All)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer notify.Stop(c)
+	for ei := range c {
+		log.Println("Got:", ei.Event(), ei.Path())
+	}
+}
 
 func main() {
-	cmd.Execute()
+	fsWatch()
+	/* cmd.Execute() */
 }
